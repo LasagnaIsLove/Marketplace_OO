948ca4870c1682653231f6c79709aa254529bef0
LasagnaIsLove <gabriel1990mota@gmail.com>
2025-06-05 21:23:37 -0300

    Implementação da logica para trocar entre a 'info' e o 'historico' do usuario

--- app/static/js/profile.js
+++ app/static/js/profile.js
@@ -3,11 +3,11 @@ document.addEventListener("DOMContentLoaded", () => {
   const inputs = form_field.querySelectorAll("input");
 
   const btn_edit = document.getElementById("btn-edit");
-  const btn_cancel = document.getElementById("btn-cancel")
+  const btn_cancel = document.getElementById("btn-cancel");
 
   function edit_toggle() {
     form_field.disabled = !form_field.disabled;
-    btn_edit.disabled = !btn_edit.disabled
+    btn_edit.disabled = !btn_edit.disabled;
     fill_inputs();
   }
 
@@ -16,11 +16,29 @@ document.addEventListener("DOMContentLoaded", () => {
       if (!input.value) {
         input.value = input.placeholder;
       } else {
-        input.value = ""
+        input.value = "";
       }
     });
   }
 
   btn_edit.addEventListener("click", edit_toggle);
-  btn_cancel.addEventListener("click", edit_toggle)
+  btn_cancel.addEventListener("click", edit_toggle);
+
+  const btnInfo = document.getElementById("btn-info");
+  const btnHistory = document.getElementById("btn-history");
+  const divInfo = document.getElementById("profile-info");
+  const divHistory = document.getElementById("profile-history");
+
+  divInfo.style.display = "block";
+  divHistory.style.display = "none";
+
+  btnHistory.addEventListener("click", () => {
+    divInfo.style.display = "none";
+    divHistory.style.display = "block";
+  });
+
+  btnInfo.addEventListener("click", () => {
+    divHistory.style.display = "none";
+    divInfo.style.display = "block";
+  });
 });
